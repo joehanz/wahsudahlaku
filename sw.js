73c3363c31c370641cad60327d09deb72bc966eb
Joehanz <yohanes2000@gmail.com>
2026-07-28 11:56:19 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
-// sw.js - File ini wajib ada supaya pembaruan berjalan
+// Pengaturan iklan & domain
+self.options = {
+  "domain": "5gvci.com",
+  "zoneId": 11439425
+};
+self.lary = "";
+
+// Memuat file inti iklan
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
+
+// Tambahan agar pembaruan berjalan lancar
 self.addEventListener('message', (event) => {
   if (event.data?.type === 'SKIP_WAITING') {
     self.skipWaiting();
